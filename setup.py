436bcb290bb2d77098df62aed977e32b87d179e3
Hong Chen <me@hongchen.cz>
2018-09-22 22:34:17 -0500

    daily commit (too lazy to type)

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
 from setuptools import setup, find_packages
 import os
 
-
 current_dir = os.path.abspath(os.path.dirname(__file__))
-
 
 with open(os.path.join(current_dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-
 setup(
-     name = 'data-util',
+     name = 'datainfo',
      version = '0.1',
-     description = 'data explorer',
-     long_description=long_description,
-     long_description_content_type='text/markdown',
-     url='https://github.com/hong-chen/data-util',
-
-     author       =' Hong Chen',
-     author_email = 'me@hongchen.cz',
-
+     description = 'List general information of datasets within a data file popular in Atmospheric Science.',
+     long_description = long_description,
+     long_description_content_type = 'text/markdown',
+     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+      ],
+     keywords = 'data info list variable utilities',
+     url    = 'https://github.com/hong-chen/datainfo',
+     author = 'Hong Chen, Yixing Shao',
+     author_email = 'me@hongchen.cz, yixingshao@foxmail.com',
      license = 'MIT',
-
+     packages = ['datainfo'],
      install_requires = ['numpy', 'scipy', 'h5py', 'netcdf4', 'python-hdf4'],
-     python_requires = '>=3.3, <3.7',
+     python_requires  = '~=3.6',
+     include_package_data = True,
+     zip_safe = False
      )
